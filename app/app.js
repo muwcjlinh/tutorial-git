273a5980618aa17a200fa8f7e1a9a4a3a743a46b
Ryan Nguyen <vietdungnguyen0412@gmail.com>
2018-03-13 16:42:32 +0700

    add folder app
    add app.js start server

---
+++ app/app.js
@@ -0,0 +1,8 @@
+var express = require ('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log("Server listening on " + app.get('port'));
+});
