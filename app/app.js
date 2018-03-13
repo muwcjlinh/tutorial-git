24744b496f27602415a89f27c373db19c21ad6ca
Ryan Nguyen <vietdungnguyen0412@gmail.com>
2018-03-13 16:56:29 +0700

    Creat api for home

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', function(req, res) {
+    res.send("Hello world");
+})
+
 var server = app.listen(app.get('port'), function() {
     console.log("Server listening on " + app.get('port'));
 });
