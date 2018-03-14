f28e093b88d756810ff68e46068e71aeb4e1cbe4
Ryan Nguyen <vietdungnguyen0412@gmail.com>
2018-03-14 10:31:42 +0700

    update team List

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 var express = require ('express');
 var app = express();
+var data = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
+app.set('dataT', data);
 
 app.locals.siteTitle = "Git test";
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+    var data = req.app.get('dataT');
+    var indexData = data.team;
+
     res.render('index', {
         pageTitle: 'Homepage',
+        index : indexData,
         pageID: 'homepage'
     })
 });
